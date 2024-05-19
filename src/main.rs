beb282666def269b024f6c64c430d5431c8857e3
Han Wu <xjohnwu@gmail.com>
2024-05-19 14:43:38 +0800

    feat: test code for anyhow and thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use ecosystem::MyError;
+
+fn main() -> Result<(), MyError> {
+    println!("Size of my error is {}", std::mem::size_of::<MyError>());
+
+    // let filename = "non-existent-file.txt";
+    // let _fd =
+    //     std::fs::File::open(filename).with_context(|| format!("Cannot find file: {}", filename))?;
+    fail_with_error()?;
+
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
